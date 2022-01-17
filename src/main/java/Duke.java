2e7aab7a5b5c874b887589b7fe40b2b633c82277
weijuey <e0543600@u.nus.edu>
2022-01-17 15:28:59 +0800

    Added BufferedReader as way of reading from command line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Duke {
     private static String welcomeMessage = "Hi";
 
     private static String farewellMessage = "Bye";
-    
+
     public static void main(String[] args) {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
