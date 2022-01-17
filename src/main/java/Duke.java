e9134552c6eb2de60d43a60fc0a479319f617f72
weijuey <e0543600@u.nus.edu>
2022-01-17 15:26:54 +0800

    Added new welcome and farewell messages as class-level members

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 public class Duke {
+    private static String welcomeMessage = "Hi";
+
+    private static String farewellMessage = "Bye";
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
