b1b1faf5e80752b9e67dc57ba464d1fbbed1d68e
weijuey <e0543600@u.nus.edu>
2022-01-19 18:03:24 +0800

    Implemented Deadline task type

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Deadline {
+public class Deadline extends Task {
+    protected String deadline;
+
+    public Deadline(String descriptor, String deadline) {
+        super(descriptor);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[D] %s (by %s)", super.toString(), deadline);
+    }
 }
