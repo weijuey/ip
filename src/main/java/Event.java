9838447f0ef8ddc18a885bc7954c82099e13dfff
weijuey <e0543600@u.nus.edu>
2022-01-19 18:03:02 +0800

    Implemented Event task type.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,13 @@
-public class Event {
+public class Event extends Task {
+    protected String duration;
+
+    public Event(String descriptor, String duration) {
+        super(descriptor);
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E] %s (at %s)", super.toString(), duration);
+    }
 }
